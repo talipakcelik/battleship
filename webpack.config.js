c10f8662fa86bf7917067956fc13c4bed6e54a73
Talip <talipakcelik@gmail.com>
2022-03-01 21:07:42 +0300

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: '[name][ext]',
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    open: true,
+    hot: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  // loaders
+  module: {
+    rules: [
+      // css
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      // images
+      { test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/, type: 'asset/resource' },
+      // babel
+      {
+        test: /\.m?js$/,
+        // exclude node_modules from test
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+  //plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'battleship',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/temp.html'),
+      // favicon: "",
+    }),
+  ],
+};
