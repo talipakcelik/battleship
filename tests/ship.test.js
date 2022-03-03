a4cce04634b1302c00f295b4c3f23f7149420a94
Talip <talipakcelik@gmail.com>
2022-03-03 18:58:46 +0300

    Add test "hit method marks correct position as hit in ship array"

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -4,3 +4,9 @@ test('ship length', function () {
   const ship1 = Ship(4);
   expect(ship1.lengthOfShip).toEqual(4);
 });
+
+test('hit method marks correct position as hit in ship array', function () {
+  const ship2 = Ship(5);
+  ship2.hit(3);
+  expect(ship2.position.at(3)).toEqual('X');
+});
