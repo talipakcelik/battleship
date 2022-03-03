71e5bf5b83ab4a733332875971f46ceaa95ffa6e
Talip <talipakcelik@gmail.com>
2022-03-03 23:00:04 +0300

    Add test isSunk method turns sunk property to true value when ships sink

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -10,3 +10,12 @@ test('hit method marks correct position as hit in ship array', function () {
   ship2.hit(3);
   expect(ship2.position.at(3)).toEqual('X');
 });
+
+test('isSunk method turns sunk property to true value when ships sink', function () {
+  const ship3 = Ship(3);
+  ship3.hit(0);
+  ship3.hit(1);
+  ship3.hit(2);
+  ship3.isSunk();
+  expect(ship3.sunk).toBe(true);
+});
