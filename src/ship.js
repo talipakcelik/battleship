fc6cfdf6cddc178af3945ae02b805b102e137cdf
Talip <talipakcelik@gmail.com>
2022-03-03 18:32:13 +0300

    Add Ship factory function

---
+++ src/ship.js
@@ -0,0 +1,27 @@
+const Ship = function (length) {
+  const ship = { length, position: Array(length).fill(null), sunk: false };
+
+  return Object.freeze({
+    get lengthOfShip() {
+      return ship.length;
+    },
+    get position() {
+      return ship.position;
+    },
+    get sunk() {
+      return ship.sunk;
+    },
+    hit(index) {
+      ship.position[index] = 'X';
+    },
+    isSunk() {
+      if (ship.position.every(el => el === 'X')) {
+        ship.sunk = true;
+      } else {
+        ship.sunk = false;
+      }
+    },
+  });
+};
+
+export { Ship };
