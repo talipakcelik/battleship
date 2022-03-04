b8a760c4802278c6973a3747b8a809efffbfe790
Talip <talipakcelik@gmail.com>
2022-03-05 00:31:54 +0300

    Add test for gameboard "gameboards receiveAttack function that takes a pair of coordinates, determines whether or not the attack hit a "

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -8,3 +8,17 @@ test('gameboard places ships at specific coordinates by calling the ship factory
   const doArraysIntersect = board1.board[3].slice(5, 9);
   expect(doArraysIntersect).toEqual(['NH', 'NH', 'NH', 'NH']);
 });
+
+test('gameboards receiveAttack function that takes a pair of coordinates, determines whether or not the attack hit a ship', function () {
+  const ship5 = Ship(6);
+  const board2 = Gameboard();
+  board2.place(2, 1, ship5.lengthOfShip, ship5.position);
+  expect(board2.receiveAttack(2, 2)).toBe(true);
+});
+
+test('gameboards receiveAttack function that takes a pair of coordinates, determines whether or not the attack hit a ship', function () {
+  const ship6 = Ship(3);
+  const board2 = Gameboard();
+  board2.place(2, 1, ship6.lengthOfShip, ship6.position);
+  expect(board2.receiveAttack(3, 1)).toBe(false);
+});
