56862dde56a91fab1bed271eed517a5faa61afa3
Talip <talipakcelik@gmail.com>
2022-03-06 15:16:08 +0300

    test: refactor for new board type

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -2,23 +2,28 @@ import { Gameboard } from '../src/gameboard';
 import { Ship } from '../src/ship';
 
 test('gameboard places ships at specific coordinates by calling the ship factory function', function () {
-  const ship4 = Ship(4);
+  const ship4 = Ship(4, 'Blue');
   const board1 = Gameboard();
-  board1.place(3, 5, ship4.lengthOfShip, ship4.position);
+  board1.place(3, 5, ship4);
   const doArraysIntersect = board1.board[3].slice(5, 9);
-  expect(doArraysIntersect).toEqual(['NH', 'NH', 'NH', 'NH']);
+  expect(doArraysIntersect).toEqual([
+    { value: 'Blue', hit: false },
+    { value: 'Blue', hit: false },
+    { value: 'Blue', hit: false },
+    { value: 'Blue', hit: false },
+  ]);
 });
 
 test('gameboards receiveAttack function that takes a pair of coordinates, determines whether or not the attack hit a ship', function () {
   const ship5 = Ship(6);
   const board2 = Gameboard();
-  board2.place(2, 1, ship5.lengthOfShip, ship5.position);
+  board2.place(2, 1, ship5);
   expect(board2.receiveAttack(2, 2)).toBe(true);
 });
 
 test('gameboards receiveAttack function that takes a pair of coordinates, determines whether or not the attack hit a ship', function () {
   const ship6 = Ship(3);
   const board2 = Gameboard();
-  board2.place(2, 1, ship6.lengthOfShip, ship6.position);
+  board2.place(2, 1, ship6);
   expect(board2.receiveAttack(3, 1)).toBe(false);
 });
