37b331bbd45855938d1a387ee1f758a5aca24b9c
Talip <talipakcelik@gmail.com>
2022-03-09 16:39:35 +0300

    Add switchPlayer functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import style from './style.css';
 import { Ship } from './ship.js';
 import { Gameboard } from './gameboard.js';
+import { Player } from './player';
+import { randomStore } from '../src/random';
+import { randomAttack } from '../src/random';
+
+function switchPlayer() {
+  if (activePlayer === player1) activePlayer = player2;
+  else activePlayer = player1;
+}
+
+const playerBoard = Gameboard();
+const enemyBoard = Gameboard();
+const player1 = Player('player1', enemyBoard);
+const player2 = Player('player2', playerBoard);
+
+let activePlayer = player1;
+
+export { switchPlayer, activePlayer, playerBoard };
