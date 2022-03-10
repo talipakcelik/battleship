342d1e2319a62ccf97029da2f8d805b0b5293c17
Talip <talipakcelik@gmail.com>
2022-03-10 18:19:09 +0300

    Add ships for each player and basic game loop

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import style from './style.css';
 import { Ship } from './ship.js';
 import { Gameboard } from './gameboard.js';
 import { Player } from './player';
-import { randomStore } from '../src/random';
+import { pairArray } from '../src/random';
 import { randomAttack } from '../src/random';
+
+const board1 = document.querySelector('.player-board');
+const board2 = document.querySelector('.enemy-board');
 
 function switchPlayer() {
   if (activePlayer === player1) activePlayer = player2;
@@ -15,6 +18,45 @@ const enemyBoard = Gameboard();
 const player1 = Player('player1', enemyBoard);
 const player2 = Player('player2', playerBoard);
 
+const carrier = Ship(5, 'Carrier');
+const carrier2 = Ship(5, 'Carrier');
+playerBoard.place(0, 0, carrier);
+enemyBoard.place(0, 0, carrier2);
+const battleship = Ship(4, 'Battleship');
+const battleship2 = Ship(4, 'Battleship');
+playerBoard.place(1, 1, battleship);
+enemyBoard.place(1, 1, battleship2);
+const destroyer = Ship(3, 'Destroyer');
+const destroyer2 = Ship(3, 'Destroyer');
+playerBoard.place(2, 2, destroyer);
+enemyBoard.place(2, 2, destroyer2);
+const submarine = Ship(3, 'Submarine');
+const submarine2 = Ship(3, 'Submarine');
+playerBoard.place(3, 3, submarine);
+enemyBoard.place(3, 3, submarine2);
+const patrol = Ship(2, 'Patrol Boat');
+const patrol2 = Ship(2, 'Patrol Boat');
+playerBoard.place(4, 4, patrol);
+enemyBoard.place(4, 4, patrol2);
+
+console.log(enemyBoard.board);
+
 let activePlayer = player1;
 
-export { switchPlayer, activePlayer, playerBoard };
+board2.addEventListener('click', function (e) {
+  player1.attack(e.target.dataset.a, e.target.dataset.b);
+  switchPlayer();
+  randomAttack(0, pairArray.length - 1, activePlayer);
+
+  switchPlayer();
+
+  if (player1.board.areShipsSunk() === true) {
+    document.querySelector(
+      '.text'
+    ).textContent = `Game over. Congratulations, you won!`;
+  } else if (player2.board.areShipsSunk() === true) {
+    document.querySelector('.text').textContent = `Game over. You lose.`;
+  }
+});
+
+export { switchPlayer, activePlayer, player1, playerBoard };
