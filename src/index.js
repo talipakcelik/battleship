a76069fad35146978058c9b09ca0caa3dee051a2
Talip <talipakcelik@gmail.com>
2022-03-10 22:10:55 +0300

    Fix computer attack which is happen after game is finished

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import { Player } from './player';
 import { pairArray } from '../src/random';
 import { randomAttack } from '../src/random';
 
-function switchPlayer() {
-  if (activePlayer === player1) activePlayer = player2;
-  else activePlayer = player1;
-}
-
 const playerBoard = Gameboard();
 const enemyBoard = Gameboard();
 const player1 = Player('player1', enemyBoard);
@@ -19,24 +14,26 @@ const carrier = Ship(5, 'Carrier');
 const carrier2 = Ship(5, 'Carrier');
 playerBoard.place(0, 0, carrier);
 enemyBoard.place(0, 0, carrier2);
+
 const battleship = Ship(4, 'Battleship');
 const battleship2 = Ship(4, 'Battleship');
 playerBoard.place(1, 1, battleship);
 enemyBoard.place(1, 1, battleship2);
+
 const destroyer = Ship(3, 'Destroyer');
 const destroyer2 = Ship(3, 'Destroyer');
 playerBoard.place(2, 2, destroyer);
 enemyBoard.place(2, 2, destroyer2);
+
 const submarine = Ship(3, 'Submarine');
 const submarine2 = Ship(3, 'Submarine');
 playerBoard.place(3, 3, submarine);
 enemyBoard.place(3, 3, submarine2);
+
 const patrol = Ship(2, 'Patrol Boat');
 const patrol2 = Ship(2, 'Patrol Boat');
 playerBoard.place(4, 4, patrol);
 enemyBoard.place(4, 4, patrol2);
-
-console.log(enemyBoard.board);
 
 let activePlayer = player1;
 
@@ -46,11 +43,15 @@ cells.forEach(cell => cell.addEventListener('click', game));
 function game(e) {
   player1.attack(e.target.dataset.a, e.target.dataset.b);
   e.target.removeEventListener('click', game);
-
   switchPlayer();
-  randomAttack(0, pairArray.length - 1, activePlayer);
 
-  switchPlayer();
+  if (
+    player1.board.areShipsSunk() === false &&
+    player2.board.areShipsSunk() === false
+  ) {
+    randomAttack(0, pairArray.length - 1, activePlayer);
+    switchPlayer();
+  }
 
   if (player1.board.areShipsSunk() === true) {
     document.querySelector(
@@ -65,5 +66,10 @@ function game(e) {
 
 function removeListener() {
   cells.forEach(cell => cell.removeEventListener('click', game));
+}
+
+function switchPlayer() {
+  if (activePlayer === player1) activePlayer = player2;
+  else activePlayer = player1;
 }
 export { switchPlayer, activePlayer, player1, playerBoard };
