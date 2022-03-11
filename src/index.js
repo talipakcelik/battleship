02c5fe2c88d08ef2835a0a61c57f94a1190577f6
Talip <talipakcelik@gmail.com>
2022-03-11 19:03:01 +0300

    Refactor for ship placement

--- src/index.js
+++ src/index.js
@@ -12,32 +12,36 @@ const enemyBoard = Gameboard();
 const player1 = Player('player1', enemyBoard);
 const player2 = Player('player2', playerBoard);
 
-const carrier = Ship(5, 'Carrier');
-const carrier2 = Ship(5, 'Carrier');
-playerBoard.place(0, 0, carrier);
-enemyBoard.place(0, 0, carrier2);
-
-const battleship = Ship(4, 'Battleship');
-const battleship2 = Ship(4, 'Battleship');
-playerBoard.place(1, 1, battleship);
-enemyBoard.place(1, 1, battleship2);
-
-const destroyer = Ship(3, 'Destroyer');
-const destroyer2 = Ship(3, 'Destroyer');
-playerBoard.place(2, 2, destroyer);
-enemyBoard.place(2, 2, destroyer2);
-
-const submarine = Ship(3, 'Submarine');
-const submarine2 = Ship(3, 'Submarine');
-playerBoard.place(3, 3, submarine);
-enemyBoard.place(3, 3, submarine2);
-
-const patrol = Ship(2, 'Patrol Boat');
-const patrol2 = Ship(2, 'Patrol Boat');
-playerBoard.place(4, 4, patrol);
-enemyBoard.place(4, 4, patrol2);
-
 let activePlayer = player1;
+
+const carrier = Ship(5, 'Carrier');
+const battleship = Ship(4, 'Battleship');
+const destroyer = Ship(3, 'Destroyer');
+const submarine = Ship(3, 'Submarine');
+const patrol = Ship(2, 'Patrol Boat');
+player2.board.place(0, 0, carrier);
+player2.board.place(1, 1, battleship);
+player2.board.place(2, 2, destroyer);
+player2.board.place(3, 3, submarine);
+player2.board.place(4, 4, patrol);
+
+switchPlayer();
+
+const carrier2 = Ship(5, 'Carrier');
+const battleship2 = Ship(4, 'Battleship');
+const destroyer2 = Ship(3, 'Destroyer');
+const submarine2 = Ship(3, 'Submarine');
+const patrol2 = Ship(2, 'Patrol Boat');
+player1.board.place(8, 0, carrier2);
+player1.board.place(1, 1, battleship2);
+player1.board.place(2, 2, destroyer2);
+player1.board.place(3, 3, submarine2);
+player1.board.place(4, 4, patrol2);
+
+console.log(player1.board);
+console.log(player2.board);
+
+switchPlayer();
 
 cells.forEach(cell => cell.addEventListener('click', game));
 
