67b9d30bf7f4e2beb73022897f72ce1f1bec712f
Talip <talipakcelik@gmail.com>
2022-03-15 03:27:35 +0300

    Add multiple if else statement for overflow problem of ship placement

--- src/draganddrop.js
+++ src/draganddrop.js
@@ -1,6 +1,3 @@
-// import interact from 'interactjs';
-// import { cells } from './index.js';
-
 const cells = document.querySelectorAll(`[data-x][data-y]`);
 
 const shipsContainer = document.querySelector('.ships-container');
@@ -13,35 +10,52 @@ const target = {
   shipLength: 0,
 };
 
+let boardCheck;
+let shipIndex;
+
 ships.forEach(ship =>
   ship.addEventListener('dragstart', function (e) {
     target['ship'] = e.target;
     target['shipLength'] = e.target.childElementCount;
+    shipIndex = parseInt(target.shipNameWithId.slice(-1));
 
-    console.log(e.target.parentElement.dataset.y);
+    if (!e.currentTarget.parentElement.classList.contains('ships-container')) {
+      let counter = e.target.parentElement.dataset.y;
 
-    let counter = e.target.parentElement.dataset.y;
+      for (let i = 0; i < target.shipLength; i++) {
+        document
+          .querySelector(
+            `[data-x='${e.target.parentElement.dataset.x}'][data-y='${counter}']`
+          )
+          .classList.remove('is-taken');
 
-    for (let i = 0; i < target.shipLength; i++) {
-      document
-        .querySelector(
-          `[data-x='${e.target.parentElement.dataset.x}'][data-y='${counter}']`
-        )
-        .classList.remove('is-taken');
-
-      counter++;
+        counter++;
+      }
     }
   })
 );
 
+board.addEventListener('mousedown', function (e) {
+  target['shipNameWithId'] = e.target.id;
+  console.log(e.target.id);
+});
+
+board.addEventListener('dragover', function (e) {});
+
 shipsContainer.addEventListener('mousedown', function (e) {
   target['shipNameWithId'] = e.target.id;
+  console.log(e.target.id);
 });
 
 for (const cell of cells) {
   cell.addEventListener('dragover', function (e) {
     e.preventDefault();
-    // console.log('dragover');
+    /////////////////////////////
+    if (target.shipNameWithId.substring(-1, 7) === 'carrier') {
+      boardCheck = target.shipLength + e.target.dataset.y <= 10;
+    } else if (target.shipNameWithId.substring(-1, 10) === 'battleship') {
+      boardCheck = e.target.dataset.y <= 6;
+    }
   });
   cell.addEventListener('dragenter', function (e) {
     e.preventDefault();
@@ -51,35 +65,61 @@ for (const cell of cells) {
     // console.log('dragleave');
   });
   cell.addEventListener('drop', function (e) {
-    // console.log(e.target);
-    // console.log(e.target.classList.contains('is-taken'));
+    let counter = e.target.dataset.y - shipIndex;
+
+    console.log(counter);
 
     if (
-      !e.target.classList.contains('is-taken') &&
-      !e.target.parentElement.classList.contains('ship')
+      target.shipNameWithId.substring(-1, 7) === 'carrier' &&
+      counter < 6 &&
+      counter >= 0
     ) {
-      e.target.append(target.ship);
-      target.ship.style.position = 'relative';
-      target.ship.style.top = '-1rem';
-      target.ship.style.left = '-1.5rem';
-      // e.target.classList.add('taken');
-      console.log(e.target.dataset.y);
-
-      let counter = e.target.dataset.y;
-
-      for (let i = 0; i < target.shipLength; i++) {
-        document
-          .querySelector(
-            `[data-x='${e.target.dataset.x}'][data-y='${counter}']`
-          )
-          .classList.add('is-taken');
-
-        counter++;
-      }
+      dropShip(e, counter);
+    } else if (
+      target.shipNameWithId.substring(-1, 10) === 'battleship' &&
+      counter < 7 &&
+      counter >= 0
+    ) {
+      dropShip(e, counter);
+    } else if (
+      target.shipNameWithId.substring(-1, 9) === 'destroyer' &&
+      counter < 8 &&
+      counter >= 0
+    ) {
+      dropShip(e, counter);
+    } else if (
+      target.shipNameWithId.substring(-1, 9) === 'submarine' &&
+      counter < 8 &&
+      counter >= 0
+    ) {
+      dropShip(e, counter);
+    } else if (
+      target.shipNameWithId.substring(-1, 6) === 'patrol' &&
+      counter < 9 &&
+      counter >= 0
+    ) {
+      dropShip(e, counter);
     }
   });
 }
 
-board.addEventListener('click', e => {
-  console.log(e.target.parentElement.classList.contains('ship'));
-});
+function dropShip(e, counter) {
+  document
+    .querySelector(
+      `[data-x='${e.target.dataset.x}'][data-y='${
+        e.target.dataset.y - shipIndex
+      }']`
+    )
+    .append(target.ship);
+  target.ship.style.position = 'relative';
+  target.ship.style.top = '-1rem';
+  target.ship.style.left = '-1.5rem';
+
+  for (let i = 0; i < target.shipLength; i++) {
+    document
+      .querySelector(`[data-x='${e.target.dataset.x}'][data-y='${counter}']`)
+      .classList.add('is-taken');
+
+    counter++;
+  }
+}
