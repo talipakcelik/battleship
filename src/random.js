fd3ee1e846b3be75c919246bc11d1eec24233042
Talip <talipakcelik@gmail.com>
2022-03-18 22:37:29 +0300

    Add is taken class for random ship placement

--- src/random.js
+++ src/random.js
@@ -129,6 +129,17 @@ function filterBoard(player) {
     }, []);
 }
 
+function addClassToCell(random1, random2, shipName) {
+  let counter = random2;
+  for (let i = 0; i < shipName.length; i++) {
+    document
+      .querySelector(`[data-x='${random1}'][data-y='${counter}']`)
+      .classList.add('is-taken');
+
+    counter++;
+  }
+}
+
 function placeShipRandom(
   player,
   carrier,
@@ -149,6 +160,7 @@ function placeShipRandom(
         `[data-x='${randomNumberForCarrier1}'][data-y='${randomNumberForCarrier2}']`
       )
       .append(carrierContainer);
+    addClassToCell(randomNumberForCarrier1, randomNumberForCarrier2, carrier);
   }
 
   freeRow = filterBoard(player);
@@ -168,6 +180,11 @@ function placeShipRandom(
         `[data-x='${freeRow[randomNumberForBattleship1]}'][data-y='${randomNumberForBattleship2}']`
       )
       .append(battleshipContainer);
+    addClassToCell(
+      freeRow[randomNumberForBattleship1],
+      randomNumberForBattleship2,
+      battleship
+    );
   }
 
   freeRow = filterBoard(player);
@@ -187,6 +204,11 @@ function placeShipRandom(
         `[data-x='${freeRow[randomNumberForDestroyer1]}'][data-y='${randomNumberForDestroyer2}']`
       )
       .append(destroyerContainer);
+    addClassToCell(
+      freeRow[randomNumberForDestroyer1],
+      randomNumberForDestroyer2,
+      destroyer
+    );
   }
 
   freeRow = filterBoard(player);
@@ -206,6 +228,11 @@ function placeShipRandom(
         `[data-x='${freeRow[randomNumberForSubmarine1]}'][data-y='${randomNumberForSubmarine2}']`
       )
       .append(submarineContainer);
+    addClassToCell(
+      freeRow[randomNumberForSubmarine1],
+      randomNumberForSubmarine2,
+      submarine
+    );
   }
 
   freeRow = filterBoard(player);
@@ -225,7 +252,117 @@ function placeShipRandom(
         `[data-x='${freeRow[randomNumberForPatrol1]}'][data-y='${randomNumberForPatrol2}']`
       )
       .append(patrolContainer);
+    addClassToCell(
+      freeRow[randomNumberForPatrol1],
+      randomNumberForPatrol2,
+      patrol
+    );
   }
 }
 
-export { randomAttack, pairArray, placeShipRandom };
+function pairArrayRefresher() {
+  pairArray = [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [0, 4],
+    [0, 5],
+    [0, 6],
+    [0, 7],
+    [0, 8],
+    [0, 9],
+    [1, 0],
+    [1, 1],
+    [1, 2],
+    [1, 3],
+    [1, 4],
+    [1, 5],
+    [1, 6],
+    [1, 7],
+    [1, 8],
+    [1, 9],
+    [2, 0],
+    [2, 1],
+    [2, 2],
+    [2, 3],
+    [2, 4],
+    [2, 5],
+    [2, 6],
+    [2, 7],
+    [2, 8],
+    [2, 9],
+    [3, 0],
+    [3, 1],
+    [3, 2],
+    [3, 3],
+    [3, 4],
+    [3, 5],
+    [3, 6],
+    [3, 7],
+    [3, 8],
+    [3, 9],
+    [4, 0],
+    [4, 1],
+    [4, 2],
+    [4, 3],
+    [4, 4],
+    [4, 5],
+    [4, 6],
+    [4, 7],
+    [4, 8],
+    [4, 9],
+    [5, 0],
+    [5, 1],
+    [5, 2],
+    [5, 3],
+    [5, 4],
+    [5, 5],
+    [5, 6],
+    [5, 7],
+    [5, 8],
+    [5, 9],
+    [6, 0],
+    [6, 1],
+    [6, 2],
+    [6, 3],
+    [6, 4],
+    [6, 5],
+    [6, 6],
+    [6, 7],
+    [6, 8],
+    [6, 9],
+    [7, 0],
+    [7, 1],
+    [7, 2],
+    [7, 3],
+    [7, 4],
+    [7, 5],
+    [7, 6],
+    [7, 7],
+    [7, 8],
+    [7, 9],
+    [8, 0],
+    [8, 1],
+    [8, 2],
+    [8, 3],
+    [8, 4],
+    [8, 5],
+    [8, 6],
+    [8, 7],
+    [8, 8],
+    [8, 9],
+    [9, 0],
+    [9, 1],
+    [9, 2],
+    [9, 3],
+    [9, 4],
+    [9, 5],
+    [9, 6],
+    [9, 7],
+    [9, 8],
+    [9, 9],
+  ];
+}
+
+export { randomAttack, pairArray, placeShipRandom, pairArrayRefresher };
